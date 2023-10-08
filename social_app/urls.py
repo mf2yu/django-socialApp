b6c080af5cc8b4da9b45f4386065a5ddf22d4007
mf2yu <mf2yufx@gmail.com>
2023-10-08 18:09:35 +0800

    chore: configure django logging

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.contrib import admin
-from django.urls import path,include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('mf2yusecret/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('supersecret/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
